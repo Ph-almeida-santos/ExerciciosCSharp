f49d5ff9127a78df6171222ae75dfb372c395bba
Pedro Almeida <phalmeidasantos@gmail.com>
2025-08-06 20:58:31 -0300

    Primeiro commit - exercício MakeLooper

---
+++ ExercicioLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercicioLoop
+{
+    public class Kata
+    {
+        public static Func<char> MakeLooper(string str)
+        {
+            int contador = 0;
+            return () =>
+            {
+                char letra = str[contador];
+                contador = (contador + 1) % str.Length;
+                return letra;
+            };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var looper = Kata.MakeLooper("abc");
+
+            Console.WriteLine(looper()); // a
+            Console.WriteLine(looper()); // b
+            Console.WriteLine(looper()); // c
+            Console.WriteLine(looper()); // a
+            Console.WriteLine(looper()); // b
+
+            Console.ReadLine(); // mantém o console aberto
+        }
+    }
+}
