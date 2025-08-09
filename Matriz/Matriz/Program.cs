324f5c6fcf53cee5821079123274def81a531ae0
Pedro Almeida <phalmeidasantos@gmail.com>
2025-08-09 19:18:08 -0300

    Add files via upload

---
+++ Matriz/Matriz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Matriz
+{
+    public class Kata
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(x => !b.Contains(x)).ToArray();
+        }
+    }
+}
+
