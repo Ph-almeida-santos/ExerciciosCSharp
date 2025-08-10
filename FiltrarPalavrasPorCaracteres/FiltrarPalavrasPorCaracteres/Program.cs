4f610e8d811fce08b88d14ad1869373db4c21b7e
Pedro Almeida <phalmeidasantos@gmail.com>
2025-08-10 17:44:15 -0300

    Add files via upload

---
+++ FiltrarPalavrasPorCaracteres/FiltrarPalavrasPorCaracteres/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    // Método que retorna as palavras com mais de 3 caracteres e imprime elas
+    public static void Caractere3mais(List<string> palavras)
+    {
+        var comMaisDeTres = palavras.Where(p => p.Length > 3).ToList();
+
+        Console.WriteLine("Palavras com mais de 3 caracteres:");
+        foreach (var palavra in comMaisDeTres)
+        {
+            Console.WriteLine($"- {palavra}");
+        }
+    }
+
+    static void Main()
+    {
+        List<string> palavras = new List<string>
+        {
+            "banana", "maçã", "laranja", "abacaxi", "uva", "kiwi", "morango", "cereja", "pera", "melancia"
+        };
+
+        Caractere3mais(palavras);
+    }
+}
